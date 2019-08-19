2fc649809df26307b327c7565b3d47c9acdb7101
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 20:13:58 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,7 +17,11 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  if text.scan(/\./)
+    true
+  else
+    false
+  end
 end
 
 def valid_phone_number?(phone)
