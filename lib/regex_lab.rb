07bf35a4737d9ce8eeff399fee52e9a679a88282
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 19:58:34 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-    return true if word == word.match(/\w+[aeiou]/)
+    return true if word == word.match(/^$v/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
