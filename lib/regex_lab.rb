6587c386014d03cdaa07fae5ffd5bd8480c11294
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 20:18:29 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,7 +26,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/(\d+)-(\d+)-(\d+)/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/)
+  if phone.match(/(\d+)-(\d+)-(\d+)/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/) || phone.match(/\b([0-9]{7})\b/) || phone.match(/([0-9] ?){10}/)
     return true
   else
     return false
