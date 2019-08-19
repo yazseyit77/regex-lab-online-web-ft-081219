c757165d7918ae771cf76566f8d91e19a57990c7
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 20:06:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -7,11 +7,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  if word.match(/un\w+ing/)
-    true
-  else
-    false
-  end
+   word.match(/un\w+ing/)
+
 end
 
 def words_five_letters_long(text)
