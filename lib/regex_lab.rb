3e7430a401e401836e27b540d6cabd2a0ed5c38e
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 20:17:43 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -26,7 +26,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.match(/(\d+)-(\d+)-(\d+)/)
+  if phone.match(/(\d+)-(\d+)-(\d+)/) || phone.match(/(\([0-9]{3}\)([0-9]{3}-[0-9]{4})\b)/)
     return true
   else
     return false
