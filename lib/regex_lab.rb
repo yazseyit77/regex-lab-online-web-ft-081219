23c3ead74ebc334381c0efb463a991af3bfb395d
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-19 20:15:54 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,8 +17,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
-  if (text.scan(/\./)) && (text.scan(/\w+/)[0].upcase)
+  my_text = text.scan(/\w+/)[0]
+  if (text.scan(/\./)) && (my_text == my_text.upcase)
     true
   else
     false
